82565b5e7214dc3d383c7f419987fdfa7cc42465
jacobvc <vince@yourdevelopmentteam.com>
2022-07-14 12:28:26 -0500

    Update config.h

--- NeopixelServer/esp32Neopixel/config.h
+++ NeopixelServer/esp32Neopixel/config.h
@@ -12,12 +12,14 @@
 // GPIO Port mapping
 #define BUILTIN_LED 2
 #define BUILTIN_BUTTON 0
+// GPIO Used for matrix display
 #define MATRIX_GPIO 32
 
 // UI names for the virtual LEDs
 // No more thsn 4 without updating neopixeldisplay implementation
 #define LEDS_NAMES "LED 1", "LED 2", "LED 3", "LED 4"
 
-// Port numbers and names for the physical outputs
+// Port numbers and UI names for the physical outputs
+// Limited only by number of GPIOs
 #define OUTPUTS_PORTS {13, "Audio"},   {12, "Relay 1"}, {14, "Relay  2"}, \
                       {27, "Relay 3"}, {33, "Audio 2"}
